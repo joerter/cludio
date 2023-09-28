5d1d63373d50d7ba8dea3e8771bbb84e0953b770
John Oerter <john@johnoerter.me>
2023-09-28 08:12:25 -0400

    damn it feels good to be a badass

---
+++ src/cludio/core.clj
@@ -0,0 +1,90 @@
+(ns cludio.core                                                ;; <1>
+  (:require [io.pedestal.http :as http]                  ;; <2>
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http.content-negotiation :as conneg]
+            [clojure.string :as str]
+            [clojure.data.json :as json]))         ;; <3>
+
+(def unmentionables #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin" "曹操"})
+
+(def supported-types ["text/html" "application/edn" "application/json" "text/plain"])
+
+(def content-neg-intc (conneg/negotiate-content supported-types))
+
+(defn accepted-type
+  [context]
+  (get-in context [:request :accept :field] "text/plain"))
+
+(defn transform-content
+  [body content-type]
+  (case content-type
+    "text/html"        body
+    "text/plain"       body
+    "application/edn"  (pr-str body)
+    "application/json" (json/write-str body)))
+
+(defn coerce-to
+  [response content-type]
+  (-> response
+      (update :body transform-content content-type)
+      (assoc-in [:headers "Content-Type"] content-type)))
+
+(defn ok [body]
+  {:status 200 :body body})
+
+(defn not-found []
+  {:status 404 :body "Not found\n"})
+
+(defn greeting-for [nm]
+  (cond
+    (unmentionables (str/lower-case nm)) nil
+    (empty? nm)         "Hello, world!\n"
+    :else               (str "Hello, " nm "\n")))
+
+(defn respond-hello [request]
+  (let [nm   (get-in request [:query-params :name])
+        resp (greeting-for nm)]
+    (if resp
+      (ok resp)
+      (not-found))))
+
+(def echo
+  {:name ::echo
+   :enter #(assoc % :response (ok (:request %)))})
+
+(def coerce-body
+  {:name ::coerce-body
+   :leave
+   (fn [context]
+     (cond-> context
+       (nil? (get-in context [:response :headers "Content-Type"]))                    
+       (update-in [:response] coerce-to (accepted-type context))))})
+
+(def routes
+  (route/expand-routes                                   ;; <1>
+   #{["/greet" :get [coerce-body content-neg-intc respond-hello] :route-name :greet]
+     [ "/echo" :get echo ]})) ;; <2>
+
+(def service-map
+  {::http/routes routes
+   ::http/type   :jetty
+   ::http/port   8890})
+
+(defn start []
+  (http/start (http/create-server service-map)))
+
+                                                                                        ;; For interactive development
+(defonce server (atom nil))                                                             ;; <1>
+
+(defn start-dev []
+  (reset! server                                                                        ;; <2>
+          (http/start (http/create-server
+                       (assoc service-map
+                              ::http/join? false)))))                                   ;; <3>
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []                                                                        ;; <4>
+  (stop-dev)
+  (start-dev))
