3dfc6d405ccdb3a4d992a215a6198d0bc0c60100
John Oerter <john@johnoerter.me>
2023-10-16 17:04:15 -0400

    Migrations test.

--- src/cludio/core.clj
+++ src/cludio/core.clj
@@ -1,18 +1,33 @@
 (ns cludio.core
-  (:require [cludio.config :as config]
-            [com.stuartsierra.component :as component]
-            [cludio.components.example-component :as example-component]
-            [cludio.components.pedestal-component :as pedestal-component]
-            [cludio.components.in-memory-state-component :as in-memory-state-component]
-            [next.jdbc.connection :as connection])
-  (:import (com.zaxxer.hikari HikariDataSource)))
+  (:require
+   [clojure.tools.logging :as log]
+   [cludio.components.example-component :as example-component]
+   [cludio.components.in-memory-state-component :as in-memory-state-component]
+   [cludio.components.pedestal-component :as pedestal-component]
+   [cludio.config :as config]
+   [com.stuartsierra.component :as component]
+   [next.jdbc.connection :as connection])
+  (:import
+   (com.zaxxer.hikari HikariDataSource)
+   [org.flywaydb.core Flyway]))
+
+(defn datasource-component [config]
+  (connection/component HikariDataSource (assoc (:db-spec config)
+                                                :init-fn (fn [datasource]
+                                                           (log/info "Running Database init")
+                                                           (.migrate
+                                                            (.. (Flyway/configure)
+                                                                (dataSource datasource)
+                                                                (locations (into-array String ["classpath:database/migrations"]))
+                                                                (table "schema_version")
+                                                                (load)))))))
 
 (defn api-system
   [config]
   (component/system-map
    :example-component (example-component/new-example-component config)
    :in-memory-state-component (in-memory-state-component/new-in-memory-state-component config)
-   :data-source (connection/component HikariDataSource (:db-spec config))
+   :data-source (datasource-component config)
    :pedestal-component (component/using (pedestal-component/new-pedestal-component config)
                                         [:example-component :data-source :in-memory-state-component])))
 
---
+++ test/persistence/api/migrations_test.clj
@@ -0,0 +1,51 @@
+(ns persistence.api.migrations-test
+  (:require
+   [clojure.test :refer :all]
+   [cludio.core :as core]
+   [com.stuartsierra.component :as component]
+   [next.jdbc :as jdbc]
+   [next.jdbc.result-set :as rs])
+  (:import
+   (org.testcontainers.containers PostgreSQLContainer)))
+
+(defmacro with-system
+  [[bound-var binding-expr] & body]
+  `(let [~bound-var (component/start ~binding-expr)]
+     (try
+       ~@body
+       (finally
+         (component/stop ~bound-var)))))
+
+(defn datasource-only-system
+  [config]
+  (component/system-map
+   :datasource (core/datasource-component config)))
+
+(defn create-database-container
+  []
+  (PostgreSQLContainer. "postgres:15.4"))
+
+(deftest migrations-test
+  (let [database-container (create-database-container)]
+    (try
+      (.start database-container)
+      (with-system
+        [sut (datasource-only-system
+               {:db-spec {:jdbcUrl (.getJdbcUrl database-container)
+                          :username (.getUsername database-container)
+                          :password (.getPassword database-container)}})]
+        (let [{:keys [datasource]} sut
+              [schema-version :as schema-versions]
+              (jdbc/execute!
+                (datasource)
+                ["select * from schema_version"]
+                {:builder-fn rs/as-unqualified-lower-maps})]
+          (is (= 1 (count schema-versions)))
+          (is (= {:description "add todo tables"
+                  :script "V1__add_todo_tables.sql"
+                  :success true}
+                 (select-keys schema-version [:description :script :success])))))
+      (finally
+        (.stop database-container)))))
+
+(comment (run-tests))
