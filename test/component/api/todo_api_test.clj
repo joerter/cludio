4fa8b933c381fc3a2324770a5fad868a4c628468
John Oerter <john@johnoerter.me>
2023-10-18 08:07:39 -0400

    404 test

--- test/component/api/todo_api_test.clj
+++ test/component/api/todo_api_test.clj
@@ -57,13 +57,13 @@
           (is (= 200 status))
           (is (some? (:created-at body)))
           (is (= {:todo-id (str todo-id) :title title} (select-keys body [:todo-id :title]))))
-        (comment (testing "Empty body is returned for not found todo id"
+        (testing "Empty body is returned for not found todo id"
                    (let [expected {:body "" :status 404}
-                         actual (-> (sut->url sut (url-for :get-todo
+                         actual (-> (sut->url sut (url-for :db-get-todo
                                                            {:path-params {:todo-id (random-uuid)}}))
                                     (client/get {:throw-exceptions false})
                                     (select-keys [:body :status]))]
-                     (is (= expected actual))))))
+                     (is (= expected actual)))))
       (finally
         (.stop database-container)))))
 
