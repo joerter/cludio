e044db43c33721dd0bc593204947920c5b367268
John Oerter <john@johnoerter.me>
2023-10-22 12:34:52 -0400

    Header!

--- src/cludio/ui/app_shell.clj
+++ src/cludio/ui/app_shell.clj
@@ -85,6 +85,80 @@
   [:main {:class "py-10"}
    [:div {:class "px-4 sm:px-6 lg:px-8"}]])
 
+(def open-sidebar
+  [:button {:type "button"
+            :class "-m-2.5 p-2.5 text-gray-700 lg:hidden"}
+   [:span {:class "sr-only"} "Open sidebar"]
+   [:svg {:class "h-6 w-6"
+          :fill "none"
+          :viewBox "0 0 24 24"
+          :stroke-width "1.5"
+          :stroke "currentColor"
+          :aria-hidden "true"}
+    [:path {:stroke-linecap "round"
+            :stroke-linejoin "round"
+            :d "M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"}]]])
+
+(def separator
+  [:div {:class "h-6 w-px bg-gray-900/10 lg:hidden" :aria-hidden "true"}])
+
+(def header
+  [:div {:class "flex flex-1 gap-x-4 self-stretch lg:gap-x-6"}
+   [:form {:class "relative flex flex-1 m-0"
+           :action "#"
+           :method "GET"}
+    [:label {:for "search-field"
+             :class "sr-only"} "Search"]
+    icons/search
+    [:input {:id "search-field"
+             :class "block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
+             :placeholder "Search..."
+             :type "search"
+             :name "search"}]]
+   [:div {:class "flex items-center gap-x-4 lg:gap-x-6"}
+    [:button {:type "button"
+              :class "-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"}
+     [:span {:class "sr-only"} "View notifications"]
+     icons/notification-bell]
+    [:div {:class "hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10"}]
+    [:div {:class "relative"}
+     [:button {:type "button"
+               :class "-m-1.5 flex items-center p-1.5"
+               :id "user-menu-button"
+               :aria-expanded "false"
+               :aria-haspopup "true"}
+      [:span {:class "sr-only"} "Open user menu"]
+      [:img {:class "h-8 w-8 rounded-full bg-gray-50"
+             :src "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+             :alt ""}]
+      [:span {:class "hidden lg:flex lg:items-center"}
+       [:span {:class "ml-4 text-sm font-semibold leading-6 text-gray-900"
+               :aria-hidden "true"} "Tom Cook"]
+       icons/chevron-down]
+      [:div {:class "absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
+             :role "menu"
+             :aria-orientation "vertical"
+             :aria-labelledby "user-menu-button"
+             :tabindex "-1"}
+       [:a {:href "#"
+            :class "block px-3 py-1 text-sm leading-6 text-gray-900"
+            :role "menuitem"
+            :tabindex "-1"
+            :id "user-menu-item-0"} "Your profile"]
+       [:a {:href "#"
+            :class "block px-3 py-1 text-sm leading-6 text-gray-900"
+            :role "menuitem"
+            :tabindex "-1"
+            :id "user-menu-item-1"} "Sign out"]]]]]])
+
+(def right-side
+  [:div {:class "lg:pl-72"}
+   [:div {:class "sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8"}
+    open-sidebar
+    separator
+    header]
+   content])
+
 (def sections
   [{:name "Dashboard" :link "#" :icon icons/home :isActive true}
    {:name "Calendar" :link "#" :icon icons/calendar :isActive false}
@@ -97,6 +171,6 @@
   []
   [:div
    (desktop-sidebar sections studios)
-   content])
+   right-side])
 
 (comment (render))
--- src/cludio/ui/icons.clj
+++ src/cludio/ui/icons.clj
@@ -32,3 +32,32 @@
    [:path {:stroke-linecap "round"
            :stroke-linejoin "round"
            :d "M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"}]])
+
+(def search
+  [:svg {:class "pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
+         :viewBox "0 0 20 20"
+         :fill "currentColor"
+         :aria-hidden "true"}
+   [:path {:fill-rule "evenodd"
+           :d "M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+           :clip-rule "evenodd"}]])
+
+(def notification-bell
+  [:svg {:class "h-6 w-6"
+         :fill "none"
+         :viewBox "0 0 24 24"
+         :stroke-width "1.5"
+         :stroke "currentColor"
+         :aria-hidden "true"}
+   [:path {:stroke-linecap "round"
+           :stroke-linejoin "round"
+           :d "M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"}]])
+
+(def chevron-down
+  [:svg {:class "ml-2 h-5 w-5 text-gray-400"
+         :viewBox "0 0 20 20"
+         :fill "currentColor"
+         :aria-hidden "true"}
+   [:path {:fill-rule "evenodd"
+           :d "M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+           :clip-rule "evenodd"}]])
