a7300179a4b5a3822d0246fb11ad9a5439ee04d8
John Oerter <john@johnoerter.me>
2023-11-18 00:21:56 -0500

    Some stuff to maybe change

--- src/cludio/routes/app/calendar.clj
+++ src/cludio/routes/app/calendar.clj
@@ -26,7 +26,7 @@
     (conj (concat january february) december)))
 
 (defn previous-month-days
-  "The number of days from the previous month to show based on the first day of the month and a 42 day month view.
+  "The number of days from the previous month to show based on the first day of the month and a 35 day month view.
   The Java Date-Time API uses integer values of 1-7 for days of the week starting with Monday.
   However, we want to show Sunday as the first day of the week"
   [first-day-of-month]
@@ -34,7 +34,7 @@
     (if (= 7 day-of-week) 0 day-of-week)))
 
 (defn next-month-days
-  "The number of days from the next month to show based on the last day of the month and a 42 day month view.
+  "The number of days from the next month to show based on the last day of the month and a 35 day month view.
   The Java Date-Time API uses integer values of 1-7 for days of the week starting with Monday.
   However, we want to show Sunday as the first day of the week"
   [last-day-of-month]
@@ -49,17 +49,33 @@
       (= 6 day-of-week) 0)))
 
 (defn month-view-start-and-end
-  "The start and end dates based for a 42 day calendar view for a given year and month"
+  "The start and end dates based for a 35 day calendar view for a given year and month"
   [year month]
   (let [first-day-of-month (jt/local-date year month 1)
         last-day-of-month (jt/adjust first-day-of-month :last-day-of-month)
         start-date (jt/minus first-day-of-month (jt/days (previous-month-days first-day-of-month)))
         end-date (jt/plus last-day-of-month (jt/days (next-month-days last-day-of-month)))]
-    [start-date end-date]))
-(month-view-start-and-end 2023 12)
+    [first-day-of-month last-day-of-month start-date end-date]))
+
+(defn month-view-days
+  "Iterate through all days in this month view to create map of local-date
+  is-current-month is-today"
+  [first-day-of-month last-day-of-month start end]
+  (loop [all-days (jt/iterate jt/plus start (jt/days 1))
+         result []]
+    (let [next-day (first all-days)]
+      (if (jt/after? next-day end)
+        result
+        (recur (rest all-days)
+               (conj result {:local-date next-day
+                             :is-current-month (every? true?
+                                                       [(jt/after? (jt/plus next-day (jt/days 1)) first-day-of-month) (jt/before? (jt/minus next-day (jt/days 1)) last-day-of-month)])}))))))
 
 (defn generate-month [year month]
-  (let [[start end] (month-view-start-and-end year month)]))
+  (let [[first-day-of-month last-day-of-month start end] (month-view-start-and-end year month)]
+    (month-view-days first-day-of-month last-day-of-month start end)))
+
+(generate-month 2023 10)
 
 (defn root [days]
   [:div {:class "lg:flex lg:h-full lg:flex-col"}
--- test/routes/app/calendar_test.clj
+++ test/routes/app/calendar_test.clj
@@ -4,9 +4,13 @@
             [java-time.api :as jt]))
 
 (deftest month-view-start-and-end
-  (testing "returns correct start and end day for 42 day calendar view"
+  (testing "returns correct start and end day for 35 day calendar view"
     (is (= [(jt/local-date 2023 10 1) (jt/local-date 2023 11 4)] (cal/month-view-start-and-end 2023 10)))
     (is (= [(jt/local-date 2023 10 29) (jt/local-date 2023 12 2)] (cal/month-view-start-and-end 2023 11)))
     (is (= [(jt/local-date 2023 11 26) (jt/local-date 2024 1 6)] (cal/month-view-start-and-end 2023 12)))))
+
+(deftest generate-month
+  (testing "always returns 42 days"
+    (is (= 35 (count (cal/generate-month 2023 10))))))
 
 (run-tests)
