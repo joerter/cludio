cabb3611358ddc5b1381301b52a5237cc241b75b
John Oerter <john@johnoerter.me>
2023-11-20 10:26:56 -0600

    Formatting

--- src/cludio/ui/calendar/month_view.clj
+++ src/cludio/ui/calendar/month_view.clj
@@ -77,24 +77,24 @@
       {:fill-rule "evenodd",
        :d "M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z",
        :clip-rule "evenodd"}]]]
-    [:button
-     {:type "button"
-      :class "hidden border-y border-gray-300 px-3.5 text-sm font-semibold text-gray-900 hover:bg-gray-50 focus:relative md:block"}
-     month]
-    [:span {:class "relative -mx-px h-5 w-px bg-gray-300 md:hidden"}]
-    [:a
-     {:href next-month-path
-      :class "flex h-9 w-12 items-center justify-center rounded-r-md border-y border-r border-gray-300 pl-1 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:pl-0 md:hover:bg-gray-50"}
-     [:span {:class "sr-only"} "Next month"]
-     [:svg
-      {:class "h-5 w-5",
-       :viewBox "0 0 20 20",
-       :fill "currentColor",
-       :aria-hidden "true"}
-      [:path
-       {:fill-rule "evenodd",
-        :d "M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z",
-        :clip-rule "evenodd"}]]]])
+   [:button
+    {:type "button"
+     :class "hidden border-y border-gray-300 px-3.5 text-sm font-semibold text-gray-900 hover:bg-gray-50 focus:relative md:block"}
+    month]
+   [:span {:class "relative -mx-px h-5 w-px bg-gray-300 md:hidden"}]
+   [:a
+    {:href next-month-path
+     :class "flex h-9 w-12 items-center justify-center rounded-r-md border-y border-r border-gray-300 pl-1 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:pl-0 md:hover:bg-gray-50"}
+    [:span {:class "sr-only"} "Next month"]
+    [:svg
+     {:class "h-5 w-5",
+      :viewBox "0 0 20 20",
+      :fill "currentColor",
+      :aria-hidden "true"}
+     [:path
+      {:fill-rule "evenodd",
+       :d "M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z",
+       :clip-rule "evenodd"}]]]])
 
 (defn header
   [year month next-month-path prev-month-path]
