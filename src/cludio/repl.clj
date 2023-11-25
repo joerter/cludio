c61c954c79583a27f8ac6e5f718c9013822eb768
John Oerter <john@johnoerter.me>
2023-11-24 19:35:55 -0600

    Working db test!

---
+++ src/cludio/repl.clj
@@ -0,0 +1,1 @@
+(ns cludio.repl)
--- test/routes/app/calendar_test.clj
+++ test/routes/app/calendar_test.clj
@@ -2,7 +2,9 @@
   (:require [clojure.test :refer :all]
             [cludio.routes.app.calendar :as cal]
             [java-time.api :as jt]
-            [cludio.test-util :as util]))
+            [cludio.test-util :as util]
+            [honey.sql :as sql]
+            [next.jdbc :as jdbc]))
 
 (deftest month-view-start
   (testing "returns the start date for month view based on 42 days and Sunday as first day of the week"
@@ -26,9 +28,34 @@
                          :username (.getUsername database-container)
                          :password (.getPassword database-container)}})]
         (let [{:keys [data-source]} sut
-              classes (cal/get-classes data-source (jt/local-date 2023 11 1) (jt/local-date 2023 11 30))]
-          (is (= 0 (count classes)))))
+              insert-class (-> {:insert-into [:class]
+                                :columns [:name :season-id]
+                                :values [["Ballet I" 1]]}
+                               (sql/format))
+              insert-class-repetition (-> {:insert-into [:class-repetition]
+                                           :columns [:day-of-the-week-id :teacher-id :class-id :time]
+                                           :values [[2 1 1 "16:00:00"]
+                                                    [4 1 1 "16:00:00"]]}
+                                          (sql/format))
+              insert-teacher (-> {:insert-into [:teacher]
+                                  :columns [:first-name :last-name :title]
+                                  :values [["Test" "Teacher" "Ms."]]}
+                                 (sql/format))
+              insert-class-schedule (-> {:insert-into [:class-schedule]
+                                         :columns [:class-id :class-repetition-id :datetime]
+                                         :values [[1 1 (jt/local-date-time 2023 9 4 16 0 0)]
+                                                  [1 2 (jt/local-date-time 2023 9 6 16 0 0)]]}
+                                        (sql/format))]
+          (jdbc/execute! (data-source) insert-class)
+          (jdbc/execute! (data-source) insert-class-repetition)
+          (jdbc/execute! (data-source) insert-teacher)
+          (jdbc/execute! (data-source) insert-class-schedule)
+          (testing "returns a map of local-date -> class coll"
+            (let [start (jt/local-date 2023 9 1)
+                  end (jt/local-date 2023 9 30)
+                  result (cal/get-classes data-source start end)]
+              (is (= 2 (count result)))))))
       (finally
         (.stop database-container)))))
 
-(run-tests)
+(comment (run-tests))
