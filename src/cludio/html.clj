f494231fdae9e7b5cea3261aed7aa9f94edc479f
John Oerter <john@johnoerter.me>
2023-11-29 08:15:53 -0500

    Figured out my styling issue

--- src/cludio/html.clj
+++ src/cludio/html.clj
@@ -7,7 +7,7 @@
                                      [:title (str "Cludio - " title)]
                                      [:meta {:charset "UTF-8"}]
                                      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
-                                     [:link {:href "/dist/output.css" :rel "stylesheet"}]]
+                                     [:link {:href "/dist/styles.css" :rel "stylesheet"}]]
    [:body {:class "h-full"} body-content
     (p/include-js "/dist/htmx.min.js" "/dist/cdn.js")]])
 
